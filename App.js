64798a5af5204a9c60c05d8b0c23ba271e14a111
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-09-15 15:59:33 +0300

    app updated

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
 import React from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TextInput
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-      <TextInput />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+  }
+
+  onPressButton() {
+    console.log("Button is pressesd");
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Type here"
+          onChangeText={text => this.setState({ text })}
+        />
+
+        <Text style={{ padding: 10, fontSize: 30, backgroundColor: "#e74c3c" }}>
+          {this.state.text}
+        </Text>
+        <TouchableHighlight onPress={this.onPressButton}>
+          <Text>Submit</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
